6ed4edf6981141cac207b82fee95da22f6d690e5
gongxujie <517526470@qq.com>
2022-10-26 14:16:03 +0800

    1

---
+++ GYToolLib/Classes/UIView+Frame.h
@@ -0,0 +1,16 @@
+//
+//  UIView+Frame.h
+//  GYToolLib
+//
+//  Created by 龚旭杰 on 2022/10/26.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (Frame)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GYToolLib/Classes/UIView+Frame.m
@@ -0,0 +1,12 @@
+//
+//  UIView+Frame.m
+//  GYToolLib
+//
+//  Created by 龚旭杰 on 2022/10/26.
+//
+
+#import "UIView+Frame.h"
+
+@implementation UIView (Frame)
+
+@end
